5c992642ca755ebacd40621ad7610c65c85f42a6
LTBurton1 <Liam.burton@hotmail.com>
2022-01-06 12:13:08 +0000

    5 + 6 Done

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ export function replaceItem(array, item, index) {
 }
 
 // Immutably remove an item at a specific position/index within an array
-export function removeItem(array, index) {}
+export function removeItem(array, index) {
+  return [...array.slice(0, index), ...array.slice(index + 1)];
+}
 
 //Objects:
 
@@ -30,7 +32,9 @@ export function removeItem(array, index) {}
 //     updateName({ name: "Abe" }, "Barbara")
 // should give back:
 //     { name: "Barbara"}
-export function updateName(object, newName) {}
+export function updateName(object, newName) {
+  return { ...object, name: newName };
+}
 
 // Immutably update the object so that the value under the "needsACupOfTea" property becomes the opposite of what it was.
 // Any other properties in the object should be maintained.

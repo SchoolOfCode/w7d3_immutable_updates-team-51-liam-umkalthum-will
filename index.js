fe4a6f52251f56ae7b4df6c2e8f1c71e97a5bea1
LTBurton1 <Liam.burton@hotmail.com>
2022-01-06 11:58:43 +0000

    1 and 2 Done

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // Arrays:
 
 // Immutably add an item to the end of an array
-export function addToEnd(array, item) {}
+export function addToEnd(array, item) {
+  return [...array, item];
+}
 
 // Immutably add an item to the beginning of an array
-export function addToBeginning(array, item) {}
+export function addToBeginning(array, item) {
+  return [item, ...array];
+}
 
 // Immutably insert an item at a specific position/index within an array
 export function insertItem(array, item, index) {}
